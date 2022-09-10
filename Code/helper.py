7ab0ac035d369fa2bdede9e23026d4c0bc09582a
roshnins <roshnins@hotmail.com>
2022-09-10 16:20:17 +0800

    Preprocessing Data

--- Code/helper.py
+++ Code/helper.py
@@ -2,3 +2,25 @@ def process_results(data):
     nested_values = ['video', 'author', 'music', 'stats', 'authorStats',
                      'challenges', 'duetInfo', 'textExtra', 'stickersOnItem']
     skip_values = ['challenges', 'duetInfo', 'textExtra', 'stickersOnItem']
+
+    # Create blank dictionary
+    flattened_data = {}
+    # Loop through each video
+    for idx, value in enumerate(data):
+        flattened_data[idx] = {}
+        # Loop through each property in each video
+        for property_id, property_value in value.items():
+            # Check if nested
+            if property_id in nested_values:
+                if property_id in skip_values:
+                    pass
+                else:
+                    # Loop through each nested property
+                    for nested_idx, nested_value in property_value.items():
+                        flattened_data[idx][property_id +
+                                            '_'+nested_idx] = nested_value
+            # If it's not nested, add it back to the flattened dictionary
+            else:
+                flattened_data[idx][property_id] = property_value
+
+    return flattened_data
--- Code/tiktok.py
+++ Code/tiktok.py
@@ -2,9 +2,21 @@
 from TikTokApi import TikTokApi as tiktok
 
 # Import JSON for export of data
-import json 
+import json
+# Import data processing helper
+from helper import process_results
 
-# Get cookie data
-verifyFP  = ""
+# Get cookie data from TikTok and set into verify variable
+verifyFp = "verify_kx2ee558_BH6fvQVi_cXHF_4lfK_Bimg_hH0lYMCV6Vm6"
+api = tiktok.get_instance(custom_verifyFp=verifyFp, use_test_endpoints=True)
 
-# Setup instance
+# Get data by hashtag 'singaporefoodfestival'
+trending = api.by_hashtag('singaporefoodfestival')
+# print(trending)
+
+# Process data
+flattened_data = process_results(trending)
+
+# Export data to json
+with open('export.json', 'w') as f:
+    json.dump(flattened_data, f)
