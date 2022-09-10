35aaafff17cfa42d20422b2d3f2159e02171d357
roshnins <roshnins@hotmail.com>
2022-09-10 15:48:31 +0800

    Adding python code

---
+++ Code/helper.py
@@ -0,0 +1,4 @@
+def process_results(data):
+    nested_values = ['video', 'author', 'music', 'stats', 'authorStats',
+                     'challenges', 'duetInfo', 'textExtra', 'stickersOnItem']
+    skip_values = ['challenges', 'duetInfo', 'textExtra', 'stickersOnItem']
---
+++ Code/tiktok.py
@@ -0,0 +1,10 @@
+# Importing the tiktok Python SDK
+from TikTokApi import TikTokApi as tiktok
+
+# Import JSON for export of data
+import json 
+
+# Get cookie data
+verifyFP  = ""
+
+# Setup instance
